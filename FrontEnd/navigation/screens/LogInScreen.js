12e2d3a29bc86d084ab6c39dc9a579d75adb2c91
lior <mitrlior@gmail.com>
2022-05-21 19:42:48 +0300

    Added signin fetch request

--- FrontEnd/navigation/screens/LogInScreen.js
+++ FrontEnd/navigation/screens/LogInScreen.js
@@ -1,31 +1,114 @@
-import React, { useState } from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput, Picker, Image} from 'react-native';
-import {customStyles, buttons, touchableOpacityStyle, imgStyle, textStyle, textInputStyle} from '../../assets/AppStyles';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  TextInput,
+  Picker,
+  ScrollView,
+} from "react-native";
+import {
+  customStyles,
+  buttons,
+  touchableOpacityStyle,
+  imgStyle,
+  textStyle,
+  textInputStyle,
+  pickerStyle,
+} from "../../assets/AppStyles";
 
+export default function SignInScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [selectedValue, setSelectedValue] = useState("hobby");
 
-export default function LogInScreen({ navigation }) {
-  const [selectedValue, getSelectedValue] = useState("hobby");
-    return (
+  const userLogin = () => {
+    console.log("Login");
+    console.log("username = " + username);
+    console.log("First name = " + firstName);
+    console.log("Last name = " + lastName);
+    console.log("Email = " + email);
+    console.log("Password = " + password);
+    console.log("Role =" + selectedValue);
+    fetch("https://birds-app-final-project.herokuapp.com/bird")
+      .then((response) => {
+        return response.json();
+      }).then(data => {console.log(data)})
+      .catch(console.log("Error signing in"));
+  };
+
+  return (
     <SafeAreaView style={customStyles.container}>
-      <View>
-        <Text style={textStyle.h1}>LogIn</Text>
-      </View>
-      <View>
-        <View style={textInputStyle.view}>
-          <Text style={textStyle.default}> Username: </Text>
-          <TextInput style={textInputStyle.default} />
+      <ScrollView>
+        <View style={imgStyle.main_image}>
+          <Text style={textStyle.h1}>Sign In</Text>
         </View>
-        <View style={textInputStyle.view}>
-          <Text style={textStyle.default}> Password: </Text>
-          <TextInput style={textInputStyle.default}/>
-        </View>      
-      </View>
-
-      <View style={[buttons.view, {marginTop: 30, width: '50%',},]}>
-        <TouchableOpacity style={touchableOpacityStyle.default}>
-        <Text style={buttons.text}>LogIn</Text>
-        </TouchableOpacity>
-      </View>
+        <View>
+          <View style={textInputStyle.view}>
+            <Text style={textStyle.default}> Username: </Text>
+            <TextInput
+              style={textInputStyle.default}
+              selectedValue={username}
+              onChangeText={(newText) => setUsername(newText)}
+            />
+          </View>
+          <View style={textInputStyle.view}>
+            <Text style={textStyle.default}> First Name: </Text>
+            <TextInput
+              style={textInputStyle.default}
+              onChangeText={(newText) => setFirstName(newText)}
+            />
+          </View>
+          <View style={textInputStyle.view}>
+            <Text style={textStyle.default}> Last Name: </Text>
+            <TextInput
+              style={textInputStyle.default}
+              onChangeText={(newText) => setLastName(newText)}
+            />
+          </View>
+          <View style={textInputStyle.view}>
+            <Text style={textStyle.default}> Email: </Text>
+            <TextInput
+              style={textInputStyle.default}
+              onChangeText={(newText) => setEmail(newText)}
+            />
+          </View>
+          <View style={textInputStyle.view}>
+            <Text style={textStyle.default}> Password: </Text>
+            <TextInput
+              style={textInputStyle.default}
+              onChangeText={(newText) => setPassword(newText)}
+            />
+          </View>
+          <View style={textInputStyle.view}>
+            <Text style={textStyle.default}> Role: </Text>
+            <Picker
+              selectedValue={selectedValue}
+              style={pickerStyle.item}
+              itemStyle={pickerStyle.default}
+              onChangeText={(itemValue, itemIndex) =>
+                setSelectedValue(itemValue)
+              }
+            >
+              <Picker.Item label="Hobby" value="hobby" />
+              <Picker.Item label="Birdwatcher" value="birdwatcher" />
+              <Picker.Item label="Admin" value="admin" />
+            </Picker>
+          </View>
+        </View>
+        <View style={buttons.main_buttons}>
+          <TouchableOpacity
+            style={touchableOpacityStyle.default}
+            onPress={userLogin}
+          >
+            <Text style={buttons.text}>Sign Me In!</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
-    );
+  );
 }
