96a464b49ed726a6e596c079649c1385bb345644
lior <mitrlior@gmail.com>
2022-06-25 15:36:31 +0300

    Changed configuration

--- Backend/code/app.py
+++ Backend/code/app.py
@@ -15,11 +15,12 @@ load_dotenv()
 
 logger.info(os.environ.get('name'))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'sqlite:///data.db'
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+
 # app.config['S3_KEY'] =  os.environ.get('S3_KEY')
 # app.config['S3_SECRET'] = os.environ.get('S3_SECRET')
 # app.config['S3_LOCATION'] = os.environ.get('S3_LOCATION')
@@ -39,7 +40,13 @@ api.add_resource(UserLoginResource, '/login/<string:username>')
 
 if __name__ == '__main__':
     from db import db
-
     db.init_app(app)
     # connect_to_s3()
+    if app.config['DEBUG']:
+
+        if app.config['DEBUG']:
+            @app.before_first_request
+            def create_table():
+                db.create_all()
+
     app.run(port=5000, debug=True, load_dotenv=True)
