51dca62a170b4b943dfeddd5f9cfc671f032440a
lior <mitrlior@gmail.com>
2022-06-25 15:55:28 +0300

    fixed DB uri

--- Backend/code/app.py
+++ Backend/code/app.py
@@ -17,7 +17,11 @@ load_dotenv()
 
 logger.info(os.environ.get('name'))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('_DATABASE_URL', 'sqlite:///data.db')
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
